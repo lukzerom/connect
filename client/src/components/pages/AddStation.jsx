b29bc36e7cfe1c3ca3efe420900909be9f7973bc
Lukasz <lukzerom@gmail.com>
2020-06-20 11:33:54 +0200

    Deleted api key from console log :)

--- client/src/components/pages/AddStation.jsx
+++ client/src/components/pages/AddStation.jsx
@@ -142,7 +142,7 @@ const AddStation = (props) => {
     const URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${streetNumber}+${streetName}+${city}+${country}&key=${
       GOOGLE_API_KEY || process.env.GOOGLE_API_KEY
     }`;
-    console.log(URL);
+
     delete axios.defaults.headers.common["x-auth-token"];
     try {
       const res = await axios.get(URL);
